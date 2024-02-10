2b526c2e12d0b2c05753906dcb24542effa9aee2
SCR-NETWORK <scrscrats@gmail.com>
2024-02-10 15:30:03 +0400

    Update all files

---
+++ website_example/config.js
@@ -0,0 +1,17 @@
+var api = "http://node1.cryptosymbiotic.com";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "pool1.cryptosymbiotic.com";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "support@cryptosymbiotic.com";
+
+var cryptonatorWidget = ["BTCS-BTC", "BTCS-USD", "BTCS-EUR", "BTCS-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorers.cryptosymbiotic.com/?hash={?}#blockchain_block";
+
+var transactionExplorer = "https://explorers.cryptosymbiotic.com/?hash={ID}#blockchain_transaction";
